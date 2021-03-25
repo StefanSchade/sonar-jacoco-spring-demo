27c0a6e39c37cb8978713f082743bb9b4635cc02
Dr. Stefan Schade <dr_stefan_schade@yahoo.com>
2021-03-25 18:12:30 +0100

    spring starter files

---
+++ src/main/java/com/example/springmultimoduleseleniumtest/SpringmultimoduleseleniumtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmultimoduleseleniumtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmultimoduleseleniumtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmultimoduleseleniumtestApplication.class, args);
+	}
+
+}
